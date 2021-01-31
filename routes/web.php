11d0c20e52500a846704075140ada132d26019e0
Harut <harut666180666@gmail.com>
2021-01-31 22:08:52 +0400

    socialite

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//GOOGLE login
+Route::get('login/google',[LoginController::class,'redirectToGoogle'])->name('login.google');
+Route::get('login/google/callback',[LoginController::class,'handleGoogleCallback']);
+
+// Route::get('/auth/redirect', function () {
+//     return Socialite::driver('github')->redirect();
+// });
+
+// Route::get('/auth/callback', function () {
+//     $user = Socialite::driver('github')->user();
+
+// });
+
+//Facebook login
+Route::get('login/facebook',[LoginController::class,'redirectToFacebook'])->name('login.facebook');
+Route::get('login/facebook/callback',[LoginController::class,'handleFacebookCallback']);
+
+//Github login
+Route::get('login/github',[LoginController::class,'redirectToGithub'])->name('login.github');
+Route::get('login/github/callback',[LoginController::class,'handleGithubCallback']);
